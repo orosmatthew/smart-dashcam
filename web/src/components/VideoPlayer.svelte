e550bd6662ce8afe316f788dd2115119060fb484
orosmatthew <orosmatthew@pm.me>
2023-03-08 13:31:36 -0500

    Add video player

---
+++ web/src/components/VideoPlayer.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import { defineCustomElements } from "@vime/core";
+    defineCustomElements();
+
+    export let thumbnail: string;
+    export let url: string;
+    export let type: string;
+</script>
+
+<vm-player controls>
+    <vm-video crossorigin="" poster={thumbnail}>
+        <source data-src={url} type={type} />
+    </vm-video>
+</vm-player>
