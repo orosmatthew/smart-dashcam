4426a1f73679fafbee7ae9a0ce85c1dc7466516c
orosmatthew <orosmatthew@pm.me>
2023-03-08 12:20:52 -0500

    Initial commit

---
+++ web/src/components/Counter.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    let count = 0;
+</script>
+
+<p>Count: {count}</p>
+
+<button
+    class="btn btn-primary"
+    on:click={() => {
+        count += 1;
+    }}>Add</button
+>
+
+<button
+    class="btn btn-secondary"
+    on:click={() => {
+        count -= 1;
+    }}>Remove</button
+>
